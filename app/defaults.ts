833c903ccf7b7095e325ab1fa60c7bb86b0a81da
Franck <gutmann.franck@outlook.fr>
2023-12-10 20:41:16 +0100

    feat: new assets

--- app/defaults.ts
+++ app/defaults.ts
@@ -9,8 +9,8 @@ export const configuration: Configuration = {
     footerTextColor: "#ffffff",
     footerText: "Made with ❤️ by Franck G.",
     favicon: "/favicon.ico",
-    icon512: "/icons/icon-512x512.png",
-    icon192: "/icons/android-chrome-192x192.png",
-    icon384: "/icons/android-chrome-384x384.png",
-    ogImage: "/og-image.png",
+    icon512: "/icons/soundbox-512.png",
+    icon192: "/icons/soundbox-192.png",
+    icon384: "/icons/soundbox-384.png",
+    ogImage: "/soundbox-og.png",
 }
